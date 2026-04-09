a3e44cc90efb1cd94e95441204109f7819c14738
Siddhant Labaje <162870099+SiddhantLabaje@users.noreply.github.com>
2026-04-09 22:19:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 // Serve frontend in production (or generally serve the built dist folder if it exists)
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/:any*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
